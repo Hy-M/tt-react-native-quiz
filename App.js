f38721b6abfa010d6e97bc03e0d8ac09d9f4205d
Hy-M <humayraa.m@outlook.com>
2020-10-13 08:38:03 +0100

    set up initial navigation

--- App.js
+++ App.js
@@ -4,10 +4,37 @@ import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Quiz from "./screens/Quiz";
+import Blog from "./screens/Blog";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const QuizStack = createStackNavigator();
+const BlogStack = createStackNavigator();
+
+const QuizStackScreen = () => {
+	return (
+		<QuizStack.Navigator>
+			<QuizStack.Screen name="Quiz" component={Quiz} />
+		</QuizStack.Navigator>
+	);
+};
+
+const BlogStackScreen = () => {
+	return (
+		<BlogStack.Navigator>
+			<BlogStack.Screen name="Blog" component={Blog} />
+		</BlogStack.Navigator>
+	);
+};
 
 export default function App() {
-	return <View></View>;
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Quiz" component={QuizStackScreen} />
+				<Tab.Screen name="Blog" component={BlogStackScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
