8d53fa7b9cbbed92c71da12367c956892362c98f
Hy-M <humayraa.m@outlook.com>
2020-10-14 16:21:55 +0100

    add accordion functionality

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,23 +1,61 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import {
+	Text,
+	View,
+	StyleSheet,
+	Button,
+	TouchableOpacity,
+	LayoutAnimation,
+} from "react-native";
+import { Icon, normalize } from "react-native-elements";
 
 export default class Accordion extends Component {
-	state = {};
+	state = {
+		expanded: false,
+	};
 
-	render() {}
+	toggleExpand = () => {
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+		this.setState({ expanded: !this.state.expanded });
+	};
+
+	render() {
+		return (
+			<View>
+				<TouchableOpacity
+					style={styles.accordionTitle}
+					onPress={() => this.toggleExpand()}
+				>
+					<Text>{this.props.title}</Text>
+					<Icon
+						type="font-awesome"
+						name={this.state.expanded ? "arrow-up" : "arrow-down"}
+						color="#50d3a7"
+						size={15}
+						style={{}}
+					/>
+				</TouchableOpacity>
+				{this.state.expanded && (
+					<View style={styles.accordionBody}>
+						<Text>{this.props.body}</Text>
+					</View>
+				)}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
+		// alignItems: "center",
+		// justifyContent: "center",
 	},
 	innerContainer: {
-		marginLeft: 10,
-		marginRight: 10,
+		// marginLeft: 10,
+		// marginRight: 10,
 	},
 	mainText: {
 		fontSize: 16,
@@ -30,14 +68,23 @@ const styles = StyleSheet.create({
 		textAlign: "center",
 		marginBottom: 20,
 	},
-	btn: {
-		backgroundColor: "#50d3a7",
-		padding: 10,
-	},
 	heading: {
 		fontSize: 32,
 		marginBottom: 40,
 		color: "#50d3a7",
 		fontWeight: "bold",
+	},
+	accordionTitle: {
+		flexDirection: "row",
+		// justifyContent: "space-between",
+		height: 56,
+		paddingLeft: 25,
+		paddingRight: 18,
+		alignItems: "center",
+		backgroundColor: "lightgrey",
+		marginBottom: 5,
+	},
+	accordionBody: {
+		padding: 15,
 	},
 });
